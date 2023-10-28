14e7cd92acbe196a87a9d2123379f2a163179067
Tulasi Deekonda <tulasideekonda@Tulasis-MacBook-Air.local>
2023-10-28 11:11:01 +0530

    First MVVM Example

---
+++ MVVMEx/MVVMEx/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMEx
+{
+	public class MainViewModel : BaseViewModel
+	{
+		private string _welComeText = "hello";
+		public string WelcomeText
+		{
+			get { return _welComeText; }
+			set
+			{
+				_welComeText = value;
+				OnPropertyChanged(WelcomeText);
+			}
+		}
+		public ICommand ChangeTextCommand { get; private set; }
+
+		public MainViewModel()
+		{
+			ChangeTextCommand = new Command(async => ChangeText());
+		}
+		private async Task ChangeText()
+		{
+			WelcomeText= "Welcome to Xamarin Forms";
+		}
+	}
+}
+
